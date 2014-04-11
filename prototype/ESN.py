b47a024e140eafd20edc8e787f4fab2966fca4e0
NiZiL <biasutto.t@gmail.com>
2014-04-11 17:27:20 +0200

    refactoring

--- prototype/ESN.py
+++ prototype/ESN.py
@@ -3,6 +3,161 @@ from matplotlib.pyplot import *
 import scipy.linalg
 import json, getopt, sys
 
+def main():
+    try:
+        opts, files = getopt.getopt(sys.argv[1:], "d", ["help"])
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+        sys.exit(1)
+
+    display = False
+    for opt, arg in opts:
+        if opt == '--help':
+            usage()
+            sys.exit(0)
+        elif opt == '-d':
+            display = True
+            
+    for k in range(len(files)) :
+        print '=== Running', files[k], "==="
+        
+        fd=open(files[k], 'r')
+        json_data = json.load(fd)
+        fd.close()
+
+        if json_data['data']['type'] == 'MackeyGlass' :
+            tmp = loadtxt(json_data['data']['path'])
+            data = zeros((1, len(tmp)))
+            for i in range(len(tmp)):
+                data[:, i] = tmp[i]
+
+            Ytarget, Y = runESN(json_data['esn']['K'], 
+                                json_data['esn']['N'], 
+                                json_data['esn']['L'], 
+                                json_data['esn']['seed'], 
+                                json_data['esn']['leaking_rate'], 
+                                json_data['esn']['rho_factor'], 
+                                json_data['esn']['regul_coef'],
+                                data, 
+                                json_data['data']['init_len'], 
+                                json_data['data']['train_len'], 
+                                json_data['data']['test_len'])
+
+            if display:
+                displayMackeyGlass(k, files[k], Ytarget, Y)
+                
+
+        elif json_data['data']['type'] == 'Sequence' :
+            tmp = loadtxt(json_data['data']['path'], dtype=string0)
+            data = zeros((json_data['esn']['K'], len(tmp)))
+            for i in range(len(tmp)):
+                data[:, i] = array(json_data['data']['encode'][tmp[i]])
+
+            Ytarget, Y = runESN(json_data['esn']['K'], 
+                                json_data['esn']['N'], 
+                                json_data['esn']['L'], 
+                                json_data['esn']['seed'], 
+                                json_data['esn']['leaking_rate'], 
+                                json_data['esn']['rho_factor'], 
+                                json_data['esn']['regul_coef'],
+                                data, 
+                                json_data['data']['init_len'], 
+                                json_data['data']['train_len'], 
+                                json_data['data']['test_len'])
+
+            if display:
+                displaySequence(k, files[k], Ytarget, Y)
+
+            #for j in range(json_data['esn']['K']):
+            #    print 'Precision en',chr(65 + j),':', float(bonneAttribution[j]) / attribution[j]
+            #    print 'Rappel en',chr(65 + j),':', float(bonneAttribution[j]) / appartenant[j]
+
+        else :
+            print 'Unsupported data type: ',json_data['data']['type']
+            sys.exit(2)
+
+    if display:
+        show()
+
+def displayMackeyGlass(k, title, Ytarget, Y):
+    fig = figure(k)
+    fig.clear()
+    fig.canvas.set_window_title(title)
+    subplot(211)
+    plot(Ytarget.T, 'k')
+    plot(Y.T, 'r')
+    legend(['Target', 'Prediction'])
+    subplot(212)
+    plot(Ytarget.T - Y.T, 'k')
+    legend(['Error'])
+
+def displaySequence(k, title, Ytarget, Y):
+    print_Ytarget = []
+    print_Y = []
+
+    bonneAttribution = {}
+    attribution = {}
+    appartenant = {}
+    precision = []
+    rappel = []
+    fmesure = []
+    accuracy = []
+    acc = 0
+
+    for i in range(len(Y.T)):
+        print_Ytarget.append(where(Ytarget[:, i]==1)[0][0])
+        print_Y.append(where(Y[:, i]==max(Y[:, i]))[0][0])
+
+        if not attribution.has_key(print_Y[i]):
+            attribution[print_Y[i]] = 0
+        if not bonneAttribution.has_key(print_Ytarget[i]):
+            bonneAttribution[print_Ytarget[i]] = 0
+        if not appartenant.has_key(print_Ytarget[i]):
+            appartenant[print_Ytarget[i]] = 0
+
+        appartenant[print_Ytarget[i]] +=1
+        attribution[print_Y[i]] += 1
+
+        if print_Y[i] == print_Ytarget[i]:
+            bonneAttribution[print_Y[i]]+=1
+            acc+=1
+        accuracy.append(float(acc)/(i+1))
+
+        tmpP = 0.0
+        tmpR = 0.0
+                
+        for j in range(len(Y)):
+            if bonneAttribution.has_key(j) and attribution.has_key(j) :
+                tmpP += float(bonneAttribution[j]) / ( len(Y) * attribution[j] )
+            if appartenant.has_key(j) and attribution.has_key(j):
+                tmpR += float(bonneAttribution[j]) / ( len(Y) * appartenant[j] )
+                
+        precision.append(tmpP)
+        rappel.append(tmpR)
+        if tmpP == 0 and tmpR == 0 :
+            fmesure.append(0)
+        else :
+            fmesure.append( 2*tmpP*tmpR / (tmpP + tmpR) )
+
+    k+=1
+    fig = figure(k)
+    fig.clear()
+    fig.canvas.set_window_title(title)
+    subplot(211)
+    yticks(range(26), [chr(65 + x) for x in range(26)])
+    plot(print_Ytarget, 'wo')
+    plot(print_Y, 'r+')
+    legend(['Target', 'Prediction'])
+    subplot(212)
+    plot(precision, 'g--')
+    plot(rappel, 'b--')
+    plot(fmesure, 'k')
+    plot(accuracy, 'r')
+    legend(['Precision', 'Recall', 'F-Measure', 'Accuracy'])
+    yinf, ysup = fig.get_axes()[0].get_ylim()
+    fig.get_axes()[0].set_ylim(yinf-0.5, ysup+0.5)
+
 class ESN:
     def generate(self, K, N, L, seed, leaking_rate, rho_factor):
         self.x = zeros((N, 1))
@@ -60,159 +215,9 @@ def runESN(K, N, L, seed, leaking_rate, rho_factor, regul_coef, data, initLen, t
     return data[:, trainLen+1:trainLen+testLen+1], Ymem
 
 
-def main():
-    try:
-        opts, files = getopt.getopt(sys.argv[1:], "d", ["help"])
-    except getopt.GetoptError as err:
-        print str(err)
-        usage()
-        sys.exit(1)
-
-    display = False
-    for opt, arg in opts:
-        if opt == '--help':
-            usage()
-            sys.exit(0)
-        elif opt == '-d':
-            display = True
-            
-    k = 0
-    for json_file in files :
-        fd=open(json_file, 'r')
-        json_data = json.load(fd)
-        fd.close()
-
-        print '=== Running', json_file, "==="
-
-        data = None
-        if json_data['data']['type'] == 'MackeyGlass' :
-            tmp = loadtxt(json_data['data']['path'])
-            data = zeros((1, len(tmp)))
-            for i in range(len(tmp)):
-                data[:, i] = tmp[i]
-
-            Ytarget, Y = runESN(json_data['esn']['K'], 
-                                json_data['esn']['N'], 
-                                json_data['esn']['L'], 
-                                json_data['esn']['seed'], 
-                                json_data['esn']['leaking_rate'], 
-                                json_data['esn']['rho_factor'], 
-                                json_data['esn']['regul_coef'],
-                                data, 
-                                json_data['data']['init_len'], 
-                                json_data['data']['train_len'], 
-                                json_data['data']['test_len'])
-
-            if display:
-                k+=1
-                fig = figure(k)
-                fig.clear()
-                fig.canvas.set_window_title(json_file)
-                subplot(211)
-                plot(Ytarget.T, 'k')
-                plot(Y.T, 'r')
-                legend(['Target', 'Prediction'])
-                subplot(212)
-                plot(Ytarget.T - Y.T, 'k')
-                legend(['Error'])
-
-        elif json_data['data']['type'] == 'Sequential' :
-            tmp = loadtxt(json_data['data']['path'], dtype=string0)
-            data = zeros((json_data['esn']['K'], len(tmp)))
-            for i in range(len(tmp)):
-                data[:, i] = array(json_data['data']['encode'][tmp[i]])
-
-            Ytarget, Y = runESN(json_data['esn']['K'], 
-                                json_data['esn']['N'], 
-                                json_data['esn']['L'], 
-                                json_data['esn']['seed'], 
-                                json_data['esn']['leaking_rate'], 
-                                json_data['esn']['rho_factor'], 
-                                json_data['esn']['regul_coef'],
-                                data, 
-                                json_data['data']['init_len'], 
-                                json_data['data']['train_len'], 
-                                json_data['data']['test_len'])
-
-            print_Ytarget = []
-            print_Y = []
-
-            bonneAttribution = {}
-            attribution = {}
-            appartenant = {}
-            precision = []
-            rappel = []
-            fmesure = []
-            accuracy = []
-            acc = 0
-            for i in range(json_data['data']['test_len']):
-                print_Ytarget.append(where(Ytarget[:, i]==1)[0][0])
-                print_Y.append(where(Y[:, i]==max(Y[:, i]))[0][0])
-
-                if not attribution.has_key(print_Y[i]):
-                    attribution[print_Y[i]] = 0
-                if not bonneAttribution.has_key(print_Ytarget[i]):
-                    bonneAttribution[print_Ytarget[i]] = 0
-                if not appartenant.has_key(print_Ytarget[i]):
-                    appartenant[print_Ytarget[i]] = 0
-
-                appartenant[print_Ytarget[i]] +=1
-                attribution[print_Y[i]] += 1
-
-                if print_Y[i] == print_Ytarget[i]:
-                    bonneAttribution[print_Y[i]]+=1
-                    acc+=1
-                accuracy.append(float(acc)/(i+1))
-
-                tmpP = 0.0
-                tmpR = 0.0
-                
-                for j in range(json_data['esn']['K']):
-                    if bonneAttribution.has_key(j) and attribution.has_key(j) :
-                        tmpP += float(bonneAttribution[j]) / ( json_data['esn']['K'] * attribution[j] )
-                    if appartenant.has_key(j) and attribution.has_key(j):
-                        tmpR += float(bonneAttribution[j]) / ( json_data['esn']['K'] * appartenant[j] )
-                
-                precision.append(tmpP)
-                rappel.append(tmpR)
-                if tmpP == 0 and tmpR == 0 :
-                    fmesure.append(None)
-                else :
-                    fmesure.append( 2*tmpP*tmpR / (tmpP + tmpR) )
-
-            for j in range(json_data['esn']['K']):
-                print 'Precision en',chr(65 + j),':', float(bonneAttribution[j]) / attribution[j]
-                print 'Rappel en',chr(65 + j),':', float(bonneAttribution[j]) / appartenant[j]
-
-            if display:
-                k+=1
-                fig = figure(k)
-                fig.clear()
-                fig.canvas.set_window_title(json_file)
-                subplot(211)
-                yticks(range(26), [chr(65 + x) for x in range(26)])
-                plot(print_Ytarget, 'wo')
-                plot(print_Y, 'r+')
-                legend(['Target', 'Prediction'])
-                subplot(212)
-                plot(precision, 'g--')
-                plot(rappel, 'b--')
-                plot(fmesure, 'k')
-                plot(accuracy, 'r')
-                legend(['Precision', 'Recall', 'F-Measure', '\"Accuracy\"'])
-                yinf, ysup = fig.get_axes()[0].get_ylim()
-                fig.get_axes()[0].set_ylim(yinf-0.5, ysup+0.5)
-
-        else :
-            print 'Unsupported data type: ',json_data['data']['type']
-            sys.exit(2)
-
-    if display:
-        show()
-
 
 def usage():
-    print 'python ESN.py [--help] [-d] TEST_FILE... )'
+    print 'usage: python ESN.py [--help] [-d] TEST_FILE... )'
 
 
 if __name__ == "__main__":
